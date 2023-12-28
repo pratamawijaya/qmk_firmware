043e2d9bb889d69d2338560e96b4c1d978d099bd
pratamawijaya <set.mnemonix@gmail.com>
2023-12-28 20:41:17 +0700

    fix wpm not shown

--- keyboards/sofle/keymaps/pwijayaenc/config.h
+++ keyboards/sofle/keymaps/pwijayaenc/config.h
@@ -1,6 +1,31 @@
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
+/* By default left side is selected as master, 
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
+for more options. */
+
+/* clear default encoder def */
+#undef TAP_CODE_DELAY
+
+/* encoder support */
+#define TAP_CODE_DELAY 100
+
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
